c458ef7f8597a9de7014dd8e166c628834fda34c
Mcproking <shanefoomg@gmail.com>
2025-05-08 15:07:18 +0800

    Add project files.

---
+++ structure.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+class Player {
+private:
+	string PlayerID;
+	string PlayerName;
+	bool isCheckedIn;
+
+
+public:
+};
+
+class Match {
+private:
+	string MatchID;
+	string MatchName;
+	Player* Team1;
+	Player* Team2;
+
+
+public:
+};
+
+class Viewer {
+private:
+	string 
+public:
+};
